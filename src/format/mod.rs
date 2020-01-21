097ed5828b6aead846010830a03b7729ed2f0e30
kurotakazuki <kabisakanakabi@gmail.com>
2020-01-21 12:52:17 +0900

    delate Format from Wav

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -5,9 +5,6 @@ pub mod bub;
 pub mod oao;
 pub mod wav;
 
-use crate::format::wav::Wav;
-use std::convert::From;
-
 /// Detail of the Format.
 /// This structure can be any of Blower, Bubble, Floaout and Wav.
 #[derive(Clone, Copy, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
@@ -16,15 +13,4 @@ pub struct Format {
     pub sampling_rate: u32,
     pub bits_per_sample: u16,
     pub blocks: u64
-}
-
-impl From<Wav> for Format {
-    fn from(value: Wav) -> Self {
-        Self {
-            channels: value.channels,
-            sampling_rate: value.sampling_rate,
-            bits_per_sample: value.bits_per_sample,
-            blocks: value.data_size as u64 / (value.bits_per_sample / 8) as u64 / value.channels as u64
-        }
-    }
 }
