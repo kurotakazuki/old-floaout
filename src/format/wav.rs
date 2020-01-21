e81de0e4b3b3aaef7d38290a87906e0984b47dfe
kurotakazuki <kabisakanakabi@gmail.com>
2020-01-21 14:29:34 +0900

    Wav try from Format

--- src/format/wav.rs
+++ src/format/wav.rs
@@ -1,3 +1,5 @@
+use crate::format::Format;
+use std::convert::TryFrom;
 use std::fmt;
 
 /// Details of the Wav file.
@@ -35,5 +37,32 @@ impl fmt::Display for Wav {
             self.data_size,
             self.other_size
         )
+    }
+}
+
+impl TryFrom<Format> for Wav {
+    type Error = &'static str;
+
+    fn try_from(value: Format) -> Result<Self, Self::Error> {
+        let bytes_per_sample = value.bits_per_sample / 8;
+        let riff_size = (value.channels * bytes_per_sample) as u64 * value.blocks + 36;
+        if riff_size > u32::max_value() as u64 {
+            Err("Wav riff size only accepts value no more than the largest value of u32!")
+        } else {
+            Ok(
+                Self {
+                    riff_size: riff_size as u32,
+                    format_size: 16,
+                    format_code: 3,
+                    channels: value.channels,
+                    sampling_rate: value.sampling_rate,
+                    data_rate: value.sampling_rate * (bytes_per_sample * value.channels) as u32,
+                    data_block_size: bytes_per_sample * value.channels,
+                    bits_per_sample: value.bits_per_sample,
+                    data_size: (riff_size - 36) as u32,
+                    other_size: 0
+                }
+            )
+        }
     }
 }
