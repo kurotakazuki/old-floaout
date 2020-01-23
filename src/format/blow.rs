0c5116433ee65e89207b33113086d2f58280319a
kurotakazuki <kabisakanakabi@gmail.com>
2020-01-23 17:55:23 +0900

    add Blower and Bubble structure part2

---
+++ src/format/blow.rs
@@ -0,0 +1,17 @@
+/// Details of the Blower file.
+#[derive(Clone, Copy, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Blower {
+    pub version: u8,
+    pub length: u8,
+    pub width: u8,
+    pub height: u8,
+    pub bubbles: u16,
+    pub blocks: u64,
+    pub sampling_rate: u32,
+    pub bits_per_sample: u16,
+    pub title_size: u8,
+    pub artist_size: u8,
+    pub lyrics_size: u16,
+    pub image_code: u8,
+    pub image_size: u32
+}
