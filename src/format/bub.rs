eb37f7229415dab010d9b8e52d8fd4e65df1a927
kurotakazuki <kabisakanakabi@gmail.com>
2020-01-24 23:37:05 +0900

    Bubble from Format

--- src/format/bub.rs
+++ src/format/bub.rs
@@ -1,3 +1,5 @@
+use crate::format::Format;
+
 /// Details of the Bubble file.
 #[derive(Clone, Copy, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Bubble {
@@ -12,4 +14,22 @@ pub struct Bubble {
     pub sampling_rate: u32,
     pub bits_per_sample: u16,
     pub name_size: u8
+}
+
+impl From<Format> for Bubble {
+    fn from(format: Format) -> Self {
+        Self {
+            version: 0,
+            length: 0,
+            width: 0,
+            height: 0,
+            red: 0,
+            green: 0,
+            blue: 0,
+            blocks: format.blocks,
+            sampling_rate: format.sampling_rate,
+            bits_per_sample: format.bits_per_sample,
+            name_size: 0
+        }
+    }
 }
