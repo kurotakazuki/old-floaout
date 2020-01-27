a9885bd7ac17c6e0578b2d838f68d2584541081b
kurotakazuki <kabisakanakabi@gmail.com>
2020-01-27 13:50:55 +0900

    WriteBytes trait

--- src/io/write.rs
+++ src/io/write.rs
@@ -1,8 +1,9 @@
-use std::io::{Write, Result};
+use crate::format::wav::Wav;
+use std::io::{BufWriter, Result, Write};
 
 pub trait WriteBytes<T>: Write {
-    fn write_be_bytes(&mut self, n: T) -> Result<()>;
-    fn write_le_bytes(&mut self, n: T) -> Result<()>;
+    fn write_be_bytes(&mut self, _: T) -> Result<()>;
+    fn write_le_bytes(&mut self, _: T) -> Result<()>;
 }
 
 // Maybe macro is better to write.
@@ -28,6 +29,30 @@ impl<W: Write + ?Sized> WriteBytes<f64> for W {
     #[inline]
     fn write_le_bytes(&mut self, n: f64) -> Result<()> {
         self.write_all(&n.to_le_bytes())
+    }
+}
+
+impl<W: Write + ?Sized> WriteBytes<&str> for W {
+    #[inline]
+    fn write_be_bytes(&mut self, s: &str) -> Result<()> {
+        self.write_all(s.as_bytes())
+    }
+
+    #[inline]
+    fn write_le_bytes(&mut self, s: &str) -> Result<()> {
+        self.write_all(s.chars().rev().collect::<String>().as_bytes())
+    }
+}
+
+impl<W: Write + ?Sized> WriteBytes<String> for W {
+    #[inline]
+    fn write_be_bytes(&mut self, s: String) -> Result<()> {
+        self.write_all(s.as_bytes())
+    }
+
+    #[inline]
+    fn write_le_bytes(&mut self, s: String) -> Result<()> {
+        self.write_all(s.chars().rev().collect::<String>().as_bytes())
     }
 }
 
@@ -77,4 +102,34 @@ impl<W: Write + ?Sized> WriteBytes<u64> for W {
     fn write_le_bytes(&mut self, n: u64) -> Result<()> {
         self.write_all(&n.to_le_bytes())
     }
+}
+
+pub trait WriteExt<T>: Write {
+    fn write_details(&mut self, details: T) -> Result<()>;
+}
+
+impl<W: Write> WriteExt<Wav> for BufWriter<W> {
+    #[inline]
+    fn write_details(&mut self, details: Wav) -> Result<()> {
+
+        // Riff Chunk
+        self.write_be_bytes("RIFF")?;
+        self.write_le_bytes(details.data_size + 4 + 8 + details.format_size + 8)?;
+        self.write_be_bytes("WAVE")?;
+        // Format Chunk
+        self.write_be_bytes("fmt ")?;
+        self.write_le_bytes(details.format_size)?;
+        self.write_le_bytes(details.format_code)?;
+        self.write_le_bytes(details.channels)?;
+        self.write_le_bytes(details.sampling_rate)?;
+        self.write_le_bytes(details.data_rate)?;
+        self.write_le_bytes(details.data_block_size)?;
+        self.write_le_bytes(details.bits_per_sample)?;
+        // Data Chunk
+        self.write_be_bytes("data")?;
+        self.write_le_bytes(details.data_size)?;
+
+        Ok(())
+    }
+
 }
