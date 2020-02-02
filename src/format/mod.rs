4b79f59680f02b038ab0cab08d5c820c54040cc2
kurotakazuki <kabisakanakabi@gmail.com>
2020-02-03 01:07:40 +0900

    Sample enum

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -6,3 +6,13 @@ pub mod blow;
 pub mod bub;
 pub mod oao;
 pub mod wav;
+
+/// This enum is type of sample format.
+/// It's related to Bits Per Sample.
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub enum Sample {
+    /// This type will match, if Bits Per Sample is 32.
+    Float32(f32),
+    /// This type will match, if Bits Per Sample is 64.
+    Float64(f64)
+}
